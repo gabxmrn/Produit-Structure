1b5d1b1bc30d891cbf14f4c73d5471838c2685e9
gabxmrn <149685872+gabxmrn@users.noreply.github.com>
2024-03-29 09:48:08 +0100

    love  you

--- Cours/WienerProcess.py
+++ Cours/WienerProcess.py
@@ -1,5 +1,4 @@
 
-
 from maturity import Maturity
 import numpy as np
 import pandas as pd
@@ -34,7 +33,7 @@ class WienerProcess:
             self.__dt=self.__maturity.maturity()/self.__nb_steps
             self.__z = np.random.normal(0.0,1.0,[self.__nb_simulations, self.__nb_steps])*self.__dt**0.5
     
-    def simul(self, use_dataframe:bool=True):
+    def return_simulation(self, use_dataframe:bool=True):
         """Return simulations
 
         Returns:
@@ -48,4 +47,44 @@ class WienerProcess:
                 columns=col
             )
         return x
+
+    def cumulative_simulation(self):
+        rdt=self.return_simulation(use_dataframe=True)
+        return rdt.cumsum(axis=1)
+    
+    def get_chart(self, simul_number:list):
+        cumulative_returns=self.cumulative_simulation()
+        df=cumulative_returns.loc[simul_number].T
+        self.__chart(df, "processus_Wiener")
+    
+    def __chart(self, df:pd.DataFrame, file_name:str):
+        plot=df.plot()
+        fig = plot.get_figure()
+        fig.savefig(file_name + ".png")
+    
+    def confidence_interval(self, level:float=.90, simulations:list=[]):
+        from scipy.stats import norm
+        cumylative_simulation=self.cumulative_simulation()
+        maturities=cumylative_simulation.columns
+        quantile=norm.ppf(1-(1-level)/2)
+        ic = lambda coeff:self.__drift*maturities+quantile*self.__volatility \
+                  *maturities**0.5*coeff
+        up=ic(1)
+        down=ic(-1)
+        moy=ic(0)
+        df=pd.DataFrame({
+            "Up":up,
+            "Moy":moy,
+            "Down":down
+        })
+        
+        for simulation in simulations:
+            df[f'#{simulation}']=cumylative_simulation.loc[simulation, :]
+        print(df)
+        self.__chart(df, "IC")
+        
+        
+        
+
+
         
--- Cours/main_option.py
+++ Cours/main_option.py
@@ -2,14 +2,18 @@ from WienerProcess import WienerProcess
 from maturity import Maturity
 
 
+
 maturity=Maturity(1.0)
 mvt=WienerProcess(
     drift=0,
     volatility=0.1,
     maturity=maturity,
     nb_simulations=10,
-    nb_steps=2,
+    nb_steps=250, 
     seed=272
 )
-print(mvt.simul(True))
+print(mvt.cumulative_simulation())
+print(mvt.return_simulation(True))
+#mvt.get_chart([0,1,2])
+print(mvt.confidence_interval(0.9, [1,2,3]))
 a=0
